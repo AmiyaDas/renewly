8c5766bc22f73c25b4fae186868ede097987e15a
Amiya Ranjan Das <ranjandass13@gmail.com>
2025-08-15 22:17:31 +0530

    component structure added

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Renewly</h1>
+      <p>Your journey to a sustainable lifestyle starts here.</p>
+    </div>
+  );
+};
+
+export default Home;
