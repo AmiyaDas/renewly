d2761b8f579dea04a0c228fedcef9d22bbcc3ab2
Amiya Ranjan Das <ranjandass13@gmail.com>
2025-08-15 22:26:42 +0530

    tailwind added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,8 @@ const Home = () => {
     <div>
       <h1>Welcome to Renewly</h1>
       <p>Your journey to a sustainable lifestyle starts here.</p>
+
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
