6a66df5048e947e1e2c77a6ea47affed93829ae3
Amiya Ranjan Das <ranjandass13@gmail.com>
2025-08-16 15:39:22 +0530

    hashrouter added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import AddSubscription from "./components/AddSubscription";
 import Home from "./components/Home";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SubscriptionDetails from './components/SubscriptionDetails';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import SubscriptionDetails from "./components/SubscriptionDetails";
 
 function App() {
   return (
-     <Router>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/add" element={<AddSubscription />} />
